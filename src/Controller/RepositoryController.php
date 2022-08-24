57c475af5377777e052c6f635fa6b19aa774c956
ktramis <ktramis2000@yahoo.com>
2022-08-24 11:02:47 -0400

    reception des donne du registre dans la data base

--- src/Controller/RepositoryController.php
+++ src/Controller/RepositoryController.php
@@ -4,20 +4,43 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RepositoryController extends AbstractController
+
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)//inportation de depandance
+    {
+        $this ->entityManager = $entityManager;
+    }
+    
     #[Route('/connextion', name: 'app_repository')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         //creaton de nouvelle uttilisateur $user
         $user = new User;
 
         //creation du formulaire $form
         $form= $this-> createForm(RegisterType::class,$user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted()&& $form ->isValid()){
+
+            $user=$form->getData();
+
+            //FIGE LA DATA POUR L'ENREGISTREMENT
+            $this ->entityManager->persist($user);
+
+            //execute
+            $this ->entityManager->flush();
+        }
 
         return $this->render('repository/index.html.twig',[
             'form'=>$form-> createView()
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -8,8 +8,9 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegisterType extends AbstractType
 {
@@ -18,19 +19,45 @@ class RegisterType extends AbstractType
         $builder
         
         ->add('lastname',TextType::class,[
-            'label'=>'Entre votre nom'
+            'label'=>'Entre votre nom',
+            'attr'=>[
+                'placeholder'=>'Merci de metre votre nom'
+            ]
         ])
 
         ->add('firstname',TextType::class,[
-            'label'=>'Entre votre prenom'
+            'label'=>'Entre votre prenom',
+            'attr'=>[
+                'placeholder'=>'Merci de metre votre prenom'
+            ]
         ])
     
         ->add('email',EmailType::class,[
-            'label'=>'Tape votre adresse email'
+            'label'=>'Tape votre adresse email',
+            'attr'=>[
+                'placeholder'=>'Merci de metre votre email'
+            ]
         ])
         
-        ->add('password',PasswordType::class,[
-            'label'=>'Entre votre mot de passe'
+        ->add('password',RepeatedType::class,[
+            'type'=>  PasswordType::class,
+            'invalid_message' => "le mot des passe n'est pas le meme",
+            'label' => "tape votre mot de passe",
+            'required' => true,
+            'first_options'=>[
+                'label' => "tape votre mot de passe",
+                'attr' =>
+                [
+                    'placeholder' => 'Merci de saisire votre mot de pass'
+                ]
+                ],   
+                'second_options'=>[
+                    'label' => "comfirmer mot de passe",
+                    'attr' =>
+                    [
+                        'placeholder' => 'Merci de confirmer votre mot de pass'
+                    ]
+                    ]                     
         ])
         
         ->add('submit',SubmitType::class,[
