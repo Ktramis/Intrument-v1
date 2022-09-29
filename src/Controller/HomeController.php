5fa16647282edd74e296197d1344cf81f4771e1f
ktramis <ktramis2000@yahoo.com>
2022-09-29 14:37:34 -0400

    Tamplet mise a jours image

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+
+
+    public $entityManager;
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager =$entityManager;
+    }
+
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        $produit =$this->entityManager->getRepository(Produit::class)->findAll();
+
+
+        return $this->render('home/index.html.twig',[
+            'produits'=> $produit
+        ]);
     }
 }
